a5771575ae792bea9b1dcc3304902764d79c8ebb
Masatoshi Suzuki <m.suzuki@ecei.tohoku.ac.jp>
2018-05-14 15:11:06 +0900

    Added preprocessing files

--- download.sh
+++ download.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-DATA_DIR=./data
+DIR_DATA=./data
+TYPE=jawiki
+TIMESTAMP=20151123
+FILE_ARTICLE=articles.xml.bz2
+FILE_META=metadata.xml.bz2
 
-mkdir $DATA_DIR
+mkdir -p $DIR_DATA
 
-wget https://archive.org/download/jawiki-20151123/jawiki-20151123-pages-articles.xml.bz2 -O $DATA_DIR
-wget https://archive.org/download/jawiki-20151123/jawiki-20151123-pages-meta-current.xml.bz2 -O $DATA_DIR
+wget "https://archive.org/download/$TYPE-$TIMESTAMP/$TYPE-$TIMESTAMP-pages-articles.xml.bz2" -O "$DIR_DATA/$FILE_ARTICLE"
+wget "https://archive.org/download/$TYPE-$TIMESTAMP/$TYPE-$TIMESTAMP-pages-meta-current.xml.bz2" -O "$DIR_DATA/$FILE_META"
