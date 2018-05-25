c5263ac3ebf8fb7b0cfc270f2bf784e50aa82514
Masatoshi Suzuki <m.suzuki@ecei.tohoku.ac.jp>
2018-05-25 13:58:31 +0900

    Added downdoad of dataset

--- download.sh
+++ download.sh
@@ -4,7 +4,9 @@ DIR_DATA=./data
 TYPE=jawiki
 TIMESTAMP=20151123
 FILE_ARTICLE=articles.xml.bz2
+FILE_DATASET=dataset.json
 
 mkdir -p $DIR_DATA
 
-wget "https://archive.org/download/$TYPE-$TIMESTAMP/$TYPE-$TIMESTAMP-pages-articles.xml.bz2" -O "$DIR_DATA/$FILE_ARTICLE"
+wget "https://archive.org/download/$TYPE-$TIMESTAMP/$TYPE-$TIMESTAMP-pages-articles.xml.bz2" -O $DIR_DATA/$FILE_ARTICLE
+wget "http://www.cl.ecei.tohoku.ac.jp/~m-suzuki/data/dataset.json" -O $DIR_DATA/$FILE_DATASET
