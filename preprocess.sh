6c4b3d6e820218359687d146ae8f6a9ee1bc3b0e
Masatoshi Suzuki <m.suzuki@ecei.tohoku.ac.jp>
2018-05-30 16:33:43 +0900

    Corrected path of dictionary file

--- preprocess.sh
+++ preprocess.sh
@@ -2,6 +2,7 @@
 
 DIR_DATA=./data
 DIR_WORK=./work
+DIR_DIC=./dic
 FILE_ARTICLE=articles.xml.bz2
 FILE_EXTRACT=extract.json
 FILE_EXTRACT_WITH_HYPERLINKS=extract_with_hyperlinks.json
@@ -28,9 +29,9 @@ python wikiextractor/WikiExtractor.py --processes $N_PROCESS --output - --json -
 
 # Train entity vectors
 echo "training entity vectors"
-# python scripts/make_udic.py --tokens 1000 --out $DIR_WORK/$FILE_UDIC_CSV
-# /usr/local/libexec/mecab/mecab-dict-index -d /usr/local/lib/mecab/dic/ipadic -u $DIR_WORK/$FILE_UDIC -f utf-8 -t utf-8 $DIR_WORK/$FILE_UDIC_CSV
-python scripts/tokenize_wikipedia.py --extract $DIR_WORK/$FILE_EXTRACT_WITH_HYPERLINKS --udic $DIR_WORK/$FILE_UDIC --out $DIR_WORK/$FILE_CORPUS
+# python scripts/make_udic.py --tokens 1000 --out $DIR_DIC/$FILE_UDIC_CSV
+# /usr/local/libexec/mecab/mecab-dict-index -d /usr/local/lib/mecab/dic/ipadic -u $DIR_DIC/$FILE_UDIC -f utf-8 -t utf-8 $DIR_DIC/$FILE_UDIC_CSV
+python scripts/tokenize_wikipedia.py --extract $DIR_WORK/$FILE_EXTRACT_WITH_HYPERLINKS --udic $DIR_DIC/$FILE_UDIC --out $DIR_WORK/$FILE_CORPUS
 python scripts/train_word2vec.py --corpus $DIR_WORK/$FILE_CORPUS --out $DIR_WORK/$FILE_ENTVEC --size $VECTOR_SIZE --processes $N_PROCESS
 
 # Extract features
